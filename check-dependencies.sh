83b57463acc41c62d013c2c7917ab5a27e19c0b7
Carlos Cabral <carloscabral@google.com>
2025-01-06 17:13:46 -0300

    feat: first push

---
+++ check-dependencies.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ -z "$APIGEE_PROJECT_ID" ]; then
+  echo "No APIGEE_PROJECT_ID variable set"
+  exit
+fi
+
+if [ -z "$APIGEE_KVM_ENV" ]; then
+  echo "No APIGEE_KVM_ENV variable set"
+  exit
+fi
+
+
+echo "Installing apigeecli"
+curl -s https://raw.githubusercontent.com/apigee/apigeecli/main/downloadLatest.sh | bash
+export PATH=$PATH:$HOME/.apigeecli/bin
+
+TOKEN=$(gcloud auth print-access-token)
+gcloud config set project "$APIGEE_PROJECT_ID"
+
+echo "Listing KVMs for Env $APIGEE_KVM_ENV ..."
+KVMS=$(apigeecli kvms list -e $APIGEE_KVM_ENV -o $APIGEE_PROJECT_ID -t $TOKEN)
+echo "Found KVMs for Env $APIGEE_KVM_ENV :"
+echo $KVMS
+
+echo "Exporting proxies to local temporary folder ..."
+
+mkdir ./tmp/proxies
+mkdir ./tmp/sharedflows
+cd ./tmp/proxies
+apigeecli apis export -e $APIGEE_KVM_ENV -o $APIGEE_PROJECT_ID -t $TOKEN
+
+for zipfile in *.zip; do
+  # Extract the name without the .zip extension
+  foldername="${zipfile%.*}"
+
+  # Create the directory if it doesn't exist
+  mkdir -p "$foldername"
+
+  # Unzip the file into the directory
+  unzip "$zipfile" -d "$foldername"
+
+  echo "Extracted '$zipfile' into '$foldername'"
+done
+
+cd ../sharedflows/
+apigeecli sharedflows export -e $APIGEE_KVM_ENV -o $APIGEE_PROJECT_ID -t $TOKEN
+
+for zipfile in *.zip; do
+  # Extract the name without the .zip extension
+  foldername="${zipfile%.*}"
+
+  # Create the directory if it doesn't exist
+  mkdir -p "$foldername"
+
+  # Unzip the file into the directory
+  unzip "$zipfile" -d "$foldername"
+
+  echo "Extracted '$zipfile' into '$foldername'"
+done
+
+echo "Done"
